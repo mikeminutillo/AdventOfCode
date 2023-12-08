47029a3c6eb5c6df4c3f272be8ef71a655033f8c
Mike Minutillo <mike.minutillo@particular.net>
2023-12-09 04:48:09 +0800

    Cleaning

--- src/2015/07/Day07.cs
+++ src/2015/07/Day07.cs
@@ -28,16 +28,24 @@ public class Day07 : AdventOfCodeBase<Day07>
             .Select(x => $"{x.Key}: {x.Value}")
         );
 
-    IEnumerable<Instruction> Parse(string input)
+    static IEnumerable<Instruction> Parse(string input)
         => from line in input.AsLines()
            select
-            Instruction.Parse(line, @"^(\w+) -> (\w+)", i => i[0]) ??
-            Instruction.Parse(line, @"^(\w+) AND (\w+) -> (\w+)", i => (ushort)(i[0] & i[1])) ??
-            Instruction.Parse(line, @"^(\w+) OR (\w+) -> (\w+)", i => (ushort)(i[0] | i[1])) ??
-            Instruction.Parse(line, @"^(\w+) LSHIFT (\d+) -> (\w+)", i => (ushort)(i[0] << i[1])) ??
-            Instruction.Parse(line, @"^(\w+) RSHIFT (\d+) -> (\w+)", i => (ushort)(i[0] >> i[1])) ??
-            Instruction.Parse(line, @"^NOT (\w+) -> (\w+)", i => (ushort)~i[0]) ??
+            Parse(line, @"^(\w+) -> (\w+)", i => i[0]) ??
+            Parse(line, @"^(\w+) AND (\w+) -> (\w+)", i => (ushort)(i[0] & i[1])) ??
+            Parse(line, @"^(\w+) OR (\w+) -> (\w+)", i => (ushort)(i[0] | i[1])) ??
+            Parse(line, @"^(\w+) LSHIFT (\d+) -> (\w+)", i => (ushort)(i[0] << i[1])) ??
+            Parse(line, @"^(\w+) RSHIFT (\d+) -> (\w+)", i => (ushort)(i[0] >> i[1])) ??
+            Parse(line, @"^NOT (\w+) -> (\w+)", i => (ushort)~i[0]) ??
             throw new Exception($"Unknown instruction {line}");
+
+    static Instruction? Parse(string line, string pattern, Func<ushort[], ushort> function)
+        => Regex.Match(line, pattern) is { Success: true } match
+        ? Create(match.Groups.Cast<Group>().Skip(1).Select(m => m.Value).ToArray(), function)
+        : null;
+
+    static Instruction Create(string[] wires, Func<ushort[], ushort> function)
+        => new(wires[0..^1], wires[^1], function);
 
     static IEnumerable<Instruction> Sort(Instruction[] instructions)
         => TopologicalSort([], ImmutableArray.Create(instructions));
@@ -66,13 +74,5 @@ public class Day07 : AdventOfCodeBase<Day07>
 
         public bool InputsSatisfiedBy(ImmutableArray<Instruction> inputs)
             => Inputs.All(x => ushort.TryParse(x, out var _) || inputs.Any(y => y.Output == x));
-
-        public static Instruction? Parse(string line, string pattern, Func<ushort[], ushort> function)
-            => Regex.Match(line, pattern) is { Success: true } match
-            ? Create(match.Groups.Cast<Group>().Skip(1).Select(m => m.Value).ToArray(), function)
-            : null;
-
-        static Instruction Create(string[] wires, Func<ushort[], ushort> function)
-            => new(wires[0..^1], wires[^1], function);
     }
 }
