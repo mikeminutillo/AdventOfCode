fec8f1d335ec490360e1aad4f30558ee587b7ffc
Mike Minutillo <mike.minutillo@particular.net>
2025-12-03 14:04:31 +0800

    2025 day 3

---
+++ src/2025/03/Day03.cs
@@ -0,0 +1,52 @@
+namespace AdventOfCode._2025._03;
+
+public class Day03 : AdventOfCodeBase<Day03>
+{
+    public override object? Solution1(string input) => Solve(input, 2);
+
+    public override object? Solution2(string input) => Solve(input, 12);
+
+    static decimal Solve(string input, int batteryCount)
+        => input.AsLines()
+            .Select(BatteryBank.Parse)
+            .Select(b => b.MaxJoltage(batteryCount).Dump())
+            .Sum();
+
+    record BatteryBank(int[] BatteryJoltages)
+    {
+        public decimal MaxJoltage(int digitCount)
+            => GetHighestSequenceOfDigits(BatteryJoltages.AsSpan(), digitCount)
+                .Aggregate(0m, (acc, d) => acc * 10 + d);
+
+        static IEnumerable<int> GetHighestSequenceOfDigits(Span<int> digits, int count)
+            => count switch
+            {
+                0 => [],
+                _ => MaxWithIndex(digits[..^(count - 1)]) switch
+                {
+                    (var digit, var index) => [
+                        digit, 
+                        .. GetHighestSequenceOfDigits(digits[(index + 1)..], count -1)
+                    ]
+                }
+            };
+
+        static (int digit, int index) MaxWithIndex(ReadOnlySpan<int> digits)
+        {
+            var max = -1;
+            var maxIndex = -1;
+            for (var i = 0; i < digits.Length; i++)
+            {
+                if (digits[i] > max)
+                {
+                    max = digits[i];
+                    maxIndex = i;
+                }
+            }
+            return (max, maxIndex);
+        }
+
+        public static BatteryBank Parse(string line)
+            => new([.. line.ToArray().Select(c => c - '0')]);
+    }
+}
